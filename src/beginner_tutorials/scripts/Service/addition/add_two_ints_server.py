35ec3c0ebae2745796c43d68321dee0e9789361c
Xiaoran <64033237+Xiaoran807@users.noreply.github.com>
2020-05-10 16:52:19 +0100

    tested service 'add two ints'

--- src/beginner_tutorials/scripts/Service/addition/add_two_ints_server.py
+++ src/beginner_tutorials/scripts/Service/addition/add_two_ints_server.py
@@ -4,8 +4,8 @@ from rospy_tutorials.srv import AddTwoInts,AddTwoIntsResponse
 import rospy
 
 def handle_add_two_ints(req):
-    print "Returning [%s + %s = %s]"%(req.a, req.b, (req.a + req.b))
-    return AddTwoIntsResponse(req.a + req.b)
+    print "Returning [%s + %s = %s]"%(req.a, req.b, (req.a+5))
+    return AddTwoIntsResponse(req.a)
 
 def add_two_ints_server():
     rospy.init_node('add_two_ints_server')
